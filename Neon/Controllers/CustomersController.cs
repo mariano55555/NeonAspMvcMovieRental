7d602deae994e9accbf6959a902e8f701499fdee
Jorge Mariano Paz Flores <jorge.sv@squeaky.com>
2018-04-06 14:14:36 -0600

    first commit

---
+++ Neon/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Neon.Models;
+
+namespace Neon.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = GetCustomer();
+            return View(customer);
+        }
+
+
+        public ActionResult Show(int id)
+        {
+            var customer = GetCustomer().FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+                  return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
+            {
+                 new Customer { Id= 1, Name = "Customer 1" },
+                 new Customer { Id= 2, Name = "Customer 2" }
+            };
+        }
+    }
+
+}
---
+++ Neon/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Neon.Models;
+
+namespace Neon.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "shrek" };
+
+            return View(movie);
+        }
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Show(int id)
+        {
+            var customer = GetMovies().FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+    }
+}
