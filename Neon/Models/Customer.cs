97b5c671b113bf9a5197949769d2242d16ccf2b8
Jorge Mariano Paz Flores <jorge.sv@squeaky.com>
2018-06-04 15:48:08 -0600

    Add AntiForgeryToken and custom validation

--- Neon/Models/Customer.cs
+++ Neon/Models/Customer.cs
@@ -15,6 +15,7 @@ namespace Neon.Models
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
