3c75650445df6267ff65a068e5d69815df23136e
Jorge Mariano Paz Flores <jorge.sv@squeaky.com>
2018-06-11 16:18:16 -0600

    Resftul API

---
+++ Neon/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using Neon.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Neon.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        
+        public byte MembershipTypeId { get; set; }
+    }
+}
